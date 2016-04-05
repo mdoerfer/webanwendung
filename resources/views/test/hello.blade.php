d8eb91b87d8a48b04c628e5592b4a116a9c88753
Mirjam Doerfer <mir.doerfer.14@dhbw-mosbach.de>
2016-04-05 20:51:19 +0200

    Einarbeitung in Masterlayouts. Erstellung eines Testlayouts und Verknuepfung mit der TesView

--- resources/views/test/hello.blade.php
+++ resources/views/test/hello.blade.php
@@ -1,43 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello</title>
+@extends('testlayout')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-    <style>
-        html, body {
-            height: 100%;
-        }
-
-        body {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            display: table;
-            font-weight: 100;
-            font-family: 'Lato';
-        }
-
-        .container {
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
-        }
-
-        .content {
-            text-align: center;
-            display: inline-block;
-        }
-
-        .title {
-            font-size: 96px;
-        }
-    </style>
-</head>
-<body>
-<div class="container">
-    <div class="content">
+@section('content')
         <div class="title">Here you go</div>
         <div>
             <h2>Leere Array wird durch Blade erkannt</h2>
@@ -56,8 +19,5 @@
             @foreach ($array as $person)
             <li>{{$person}}</li>
             @endforeach
-        </div>
-    </div>
-</div>
-</body>
-</html>
+
+@stop
